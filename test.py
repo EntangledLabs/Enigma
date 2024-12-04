85f8140d61187b6c33fc440dba976e25d8a56844
EntangledLabs <yiclemence@gmail.com>
2024-12-03 19:48:48 -0800

    made changes to breaking features in scoring, engine flags not properly set

--- test.py
+++ test.py
@@ -1,17 +1,19 @@
-import tomllib, tomli_w
+import tomllib, tomli_w, csv, json
+from os.path import join, splitext
 
-from enigma_requests import Box
+from enigma_requests import Box, Credlist
 
-with open('./example_configs/boxes/examplebox.toml', 'rb') as f:
-    Box.add(Box(
-        name='examplebox',
-        identifier=1,
-        config=tomli_w.dumps(tomllib.load(f))
-    ))
+cred_file = 'examplecreds2.csv'
 
-with open('./example_configs/boxes/examplebox2.toml', 'rb') as f:
-    Box.add(Box(
-        name='examplebox2',
-        identifier=2,
-        config=tomli_w.dumps(tomllib.load(f))
-    ))
+with open(join('./example_configs/creds', cred_file), 'r+') as f:
+    data = csv.reader(f)
+    creds = dict()
+    for row in data:
+        creds.update({row[0]: row[1]})
+    name = splitext(cred_file)[0].lower()
+    Credlist.add(
+        Credlist(
+            name=name,
+            creds=json.dumps(creds)
+        )
+    )
